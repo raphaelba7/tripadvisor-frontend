d24009e7ae5948d04ab23b185968a2841bf18813
Raphael Basset <ba7.raphael@gmail.com>
2024-05-02 12:08:21 +0200

    add modal / mail

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # tripadvisor-frontend
+
+**TripAdvisor copy website** made in context of training at [@Le Reacteur](https://github.com/lereacteur) bootcamp.
+
+- [x] **HTML** & **CSS** static integration
+- [x] **Responsive** : BreakPoints 1184px - 1024px - 768px - 425px
+- [x] Dynamic interaction in **Vanilla JS**
+- [x] Backend **Node JS** (contactForm) [@Github](https://github.com/raphaelba7/tripadvisor-back)
+
+## Content
+
+- Pages:
+  - Home Page : `index.html`
+
+## Tech Stack
+
+- HTML 5
+- CSS 3
+- JavaScript
+
+## Installation
+
+1. Clone the repository :
+
+```bash
+git clone https://github.com/raphaelba7/tripadvisor-frontend.git
+```
+
+2. Open `index.html` file in your browser
+
+## Thanks
+
+Thanks to :
+
+- [@Le Reacteur](https://github.com/lereacteur)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,91 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Event to display the form when click on button login
+  // Header to display border on scroll
+  const header = document.querySelector("header");
+  // Button to display the form when click on button login
   const boutonLogin = document.getElementById("buttonLogin");
+  // Button to close the form when click on button cross
+  const closeForm = document.querySelector(".icon-times");
+  // Modal
+  const modal = document.querySelector(".modal");
+  // Form to reset
+  const form = document.querySelector("form");
+  // Submit button
+  const submitButton = document.getElementById("submit-btn");
 
-  boutonLogin.addEventListener("click", () => {});
+  // Display border header on scroll
+  window.addEventListener("scroll", () => {
+    let scroll = window.scrollY;
+    if (scroll !== 0) {
+      header.classList.add("border");
+    } else {
+      header.classList.remove("border");
+    }
+  });
+
+  // Display the contact form / disable scroll.
+  boutonLogin.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    window.onscroll = function () {
+      window.scrollTo(0, 0);
+    };
+  });
+
+  // Close the contact form / enable scroll
+  closeForm.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    window.onscroll = function () {};
+  });
+
+  // Function to empty the form
+  const cleanForm = () => {
+    form.reset();
+  };
+
+  // Function to desactivate the submit button
+  const isDisabled = () => {
+    submitButton.setAttribute("disabled", "disabled");
+    submitButton.classList.add("disabled-btn");
+  };
+
+  // Function to reactivate the submit button
+  const isEnabled = () => {
+    submitButton.removeAttribute("disabled");
+    submitButton.classList.remove("disabled-btn");
+  };
 
   // Send the form when submit
   const formulaire = document.querySelector("form");
 
   formulaire.addEventListener("submit", async (event) => {
-    // pour ne pas rafraichire la page après le submit.
     event.preventDefault();
-    const firstname = document.getElementById("firstname").value;
-    const lastname = document.getElementById("lastname").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
+    isDisabled();
+    const datas = {
+      firstname: document.getElementById("firstname").value,
+      lastname: document.getElementById("lastname").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
 
-    const response = await axios.post("", {
-      firstname: firstname,
-      lastname: lastname,
-      email: email,
-      message: message,
-    });
+    //console.log(datas);
 
-    console.log(response.data);
+    try {
+      const response = await axios.post("http://localhost:3000/form", datas);
+
+      if (response.status === 200) {
+        alert("Votre formulaire a bien été envoyé");
+        cleanForm();
+        isEnabled();
+      }
+    } catch (error) {
+      if (error.response?.data.message === "Missing parameters") {
+        alert("Veuillez remplir tous les champs du formulaire");
+      } else {
+        alert("Une erreur est survenue");
+        console.log(error.response);
+        //cleanForm();
+      }
+
+      isEnabled();
+    }
   });
 });
